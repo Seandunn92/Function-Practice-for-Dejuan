273f946aa576bb05950148e3ca34c1d859333a1e
Sean Dunn <sdunn@C02W84T9HV2M.local>
2018-11-07 12:45:19 -0500

    First commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    //function 1 that prints HelloWorld (Creating)
+    public static void print(){
+        System.out.println("Hello World");
+    }
+
+    public static void main(String[] args) {
+
+        //CALLING OUR print function 1  twice!
+        print();
+        print();
+
+
+    }
+}

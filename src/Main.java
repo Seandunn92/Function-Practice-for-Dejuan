a3f944f210f2abfaef6b23309651efbd2534ffb6
Sean Dunn <sdunn@C02W84T9HV2M.local>
2018-11-07 12:59:13 -0500

    String and int print now working

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,37 @@
 public class Main {
 
-    //function 1 that prints HelloWorld (Creating)
+    //base function that prints HelloWorld (Creating)
     public static void print(){
         System.out.println("Hello World");
     }
 
+    //function 2  edits the base function to take in an int! Then it uses it!
+    public static void print(int n){
+        System.out.println("Hello " + n);
+    }
+
+    //function 3 edits the base function to take a String! Then it uses it
+    public static void print(String s){
+        System.out.println("Hello " + s);
+    }
+
     public static void main(String[] args) {
 
-        //CALLING OUR print function 1  twice!
+        //CALLING OUR baseFunction, prints "HelloWorld"
         print();
-        print();
+
+        //Calling function 2!  int n  is being defined as 3 here.  prints "Hello 3"
+        print(3);
+
+        //Calling function 2!  int n  is being defined as 102 here. prints "Hello 102"
+        print(102);
+
+        //Calling function 3! String s is being defined as Dejuan here. prints "Hello Dejuan"
+        print("Dejuan");
+        //Calling function 3! String s is being defined as Sean here. prints "Hello Sean"
+        print("Sean");
+        //
+
 
 
     }
